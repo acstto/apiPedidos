c16075861be73198a8f44034d2a8ad6d2bb556f9
acstto <acstto@gmail.com>
2025-07-11 17:18:57 -0300

    test: Criação das classes de teste unitário e testes de integração

---
+++ src/test/java/br/com/cotiinformatica/domain/PedidoServiceTest.java
@@ -0,0 +1,60 @@
+package br.com.cotiinformatica.domain;
+
+import static org.assertj.core.api.Assertions.fail;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class PedidoServiceTest {
+	
+	@BeforeEach
+	public void setUp() {
+		//TODO preparar o ambiente de testes
+	}
+	
+	@Test
+	@DisplayName("Deve criar um pedido com sucesso.")
+	public void testCriarPedidoComSucesso() {
+		//TODO implementar o teste
+		fail("Não implementado.");
+		
+	}
+	
+	@Test
+	@DisplayName("Deve alterar um pedido com sucesso.")
+	public void testAlterarPedidoComSucesso() {
+		//TODO implementar o teste
+		fail("Não implementado.");
+		
+	}
+	
+	@Test
+	@DisplayName("Deve inativar um pedido existente com sucesso.")
+	public void testInativarPedidoComSucesso() {
+		//TODO implementar o teste
+		fail("Não implementado.");
+		
+	}
+	
+	@Test
+	@DisplayName("Deve consultar um pedido com sucesso.")
+	public void testConsultarPedidoComSucesso() {
+		//TODO implementar o teste
+		fail("Não implementado.");
+		
+	}
+	
+	@Test
+	@DisplayName("Deve obter um pedido através do ID com sucesso.")
+	public void testObterPedidoPorIdComSucesso() {
+		//TODO implementar o teste
+		fail("Não implementado.");
+		
+	}
+
+
+
+
+
+}
