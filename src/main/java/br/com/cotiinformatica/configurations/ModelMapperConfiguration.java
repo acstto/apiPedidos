8c8ae6d173bf20a644016d5cb44601521e2c47ba
acstto <acstto@gmail.com>
2025-07-16 17:34:59 -0300

    fix: detalhamento da documentação do swagger

--- src/main/java/br/com/cotiinformatica/configurations/ModelMapperConfiguration.java
+++ src/main/java/br/com/cotiinformatica/configurations/ModelMapperConfiguration.java
@@ -1,4 +1,4 @@
-package br.com.cotiinformatica.configuration;
+package br.com.cotiinformatica.configurations;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
